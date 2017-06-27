418e309c41a920c65657aeed3a8f0434fc7a3c0e
Damien <damienebreaux@gmail.com>
2017-06-26 21:47:16 -0500

    stlyed the page and starting on the Js

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+// need to create an array with words in it
+var words = ["Jedi", "Staw wars", "Luke", "Han Solo", "Empire"];
+
+//choose a random word for the user to geuss
+// var wordToGeuss = words[Math.floor(Math.random()*words.length)];
+
+//  var wordGeuss = document.getElementById("word");
+//  	wordGuess.innerHTML = wordToGuess;
+var wordGuess = document.getElementById("word");
+	wordGuess.innerHTML = words[Math.floor(Math.random()*words.length)];
+
+
+
+// so its like var wordGuess = document.getElementById("word");
+
+// [8:13] 
+// with word being the id you gave the div earlier
+
+// [8:14] 
+// then do wordGuess.innerHTML = wordToGuess
+
+// [8:14] 
+// wordToGuess = the random word selected in your code
+
+
+
+
+
+//need a function that converts the array into underscores
+	//needs to loop through each word (Letter in the word) and write out the underscore when that loop is done
+
+
+// //pressed key function to get the geuss
+
+//  var letterGeussed[];
+
+//  document.onkeyup = function(event){
+//  	letterGeussed = event.key;
+//  	letterGeussed.push();
+//  }
+
+
+// //need a function that will show correcly geussed letter
+// 	//if statment where if lettergeussed[i] === words[i] then show letter 
+
+
+// //need a fuction that creates and array of wrongly geussed letters 
+// var badGeuss[];
+
+// document.write(badGeuss);
+
+// //need a win counter
+
+
+
