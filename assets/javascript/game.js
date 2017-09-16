11dfc4e4838c2660d26e1bfc5ef56b950060c717
Damien <damienebreaux@gmail.com>
2017-09-16 00:08:29 -0500

    cleaned up code for readability

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//words arry
+//words array
 var words = ["jedi", "luke"];
 //stores letters geussed
 var letterGeussed = [];
@@ -10,28 +10,23 @@ var win = 0;
 var wordToGuess = words[Math.floor(Math.random() * words.length)];
 //split random word
 var wordSplit = wordToGuess.split("");
-var secretWord = wordSplit;
 
 wordToGuess = wordToGuess.split("");
 
-
-//writting to html for Wins, Word, LetterGeuss
-document.getElementById("wins").innerHTML = win;
-document.getElementById("geussLeft").innerHTML = guessCounter;
-
-//creates arrry for random word
+//blank space post for page.
 for (var i = 0; i < wordSplit.length; i++) {
   wordSplit[i] = "_ ";
   document.getElementById("word").innerHTML = wordSplit.join("");
 }
 
-
-
 //pressed key function and into the abyss
 document.onkeyup = function(event) {
+  //writting to html for Wins, Word, LetterGeuss
+  document.getElementById("wins").innerHTML = win;
+  document.getElementById("geussLeft").innerHTML = guessCounter;
 
+  //as long as my array isnt empty code will show the key pressed.
   if (letterGeussed.length === 0) {
-    console.log("run")
     letterGeussed.push(event.key);
   }
 
@@ -40,47 +35,40 @@ document.onkeyup = function(event) {
       letterGeussed.push(event.key);
       document.getElementById("guess").innerHTML = letterGeussed;
     }
-
   }
-  // for loop to check wordToGuess and Letterguessed
+  // for loop to checks the letter you geussed. if you geuss incorrect
+  //you loose a geuss point line 49
   for (var j = 0; j < letterGeussed.length; j++) {
     for (var h = 0; h < wordToGuess.length; h++) {
       if (letterGeussed[j] === wordToGuess[h]) {
         wordSplit[h] = letterGeussed[j];
-        var hit = true;
-
+        var geussNum = true;
       }
-
     }
-
   }
-
+  //updates geuss counter per the above if geussNum eveluates to false.
+  if (!geussNum) {
+    guessCounter--;
+    document.getElementById("geussLeft").innerHTML = guessCounter;
+  }
+  //this block is the invisivle wordToGuess
+  //and is used to evaluate if the player wins
   var showWord = "";
   for (var i = 0; i < wordToGuess.length; i++) {
     if (letterGeussed.indexOf(wordToGuess[i].toLowerCase()) >= 0) {
       showWord = showWord + wordToGuess[i] + " ";
-    }
-    //need an else if to check
-    else {
+    } else {
       showWord = showWord + "_ ";
     }
   }
-
-  //checking guess
-  if (!hit) {
-    guessCounter--;
-    document.getElementById("geussLeft").innerHTML = guessCounter;
-  }
-
   document.getElementById("word").innerHTML = showWord;
-
-  console.log(showWord);
-  //loose
+  //lose
   if (guessCounter === 0) {
-    youLoose();
+    youLosse();
   }
   //win
   if (showWord.search("_") === -1) {
+    document.getElementById("word").innerHTML = showWord;
     youWin();
   }
 }
@@ -90,29 +78,29 @@ document.onkeyup = function(event) {
 function youWin() {
   win++;
   document.getElementById("wins").innerHTML = win;
-  alert("WHOOOOOOOOOO! You got'em!");
+  alert("WHOOOOOOOOOO! You got'em! Lets go again!");
   cleanSlate();
-
 }
 
 //Loose Function
-function youLoose() {
-  alert("You loose! Beware the darkside!");
+function youLose() {
+  alert("You loose! Beware the darkside! Try again.");
   cleanSlate();
 }
 
-
+//new game function
 function cleanSlate() {
-  //clears geussed letters & counter
-  document.getElementById("geussLeft").innerHTML = guessCounter;
-  document.getElementById("guess").innerHTML = letterGeussed;
   //sets up variables
-  var wordToGuess = words[Math.floor(Math.random() * words.length)];
-  var guessCounter = 10;
-  var letterGeussed = [];
-  var showWord = "";
+  wordToGuess = words[Math.floor(Math.random() * words.length)];
+  guessCounter = 10;
+  letterGeussed = [];
+  showWord = "";
   for (var i = 0; i < wordSplit.length; i++) {
     wordSplit[i] = "_ ";
     document.getElementById("word").innerHTML = wordSplit.join("");
   }
+
+  //clears geussed letters & counter
+  document.getElementById("geussLeft").innerHTML = guessCounter;
+  document.getElementById("guess").innerHTML = letterGeussed;
 }
